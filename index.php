2ea31e2ee8f989ce05d4b4385e6f9621eaa5589f
Jeremy <darksyn@gmail.com>
2015-11-05 11:13:03 -0700

    Added web font
    Added poiret one google font to site, updated styling for font
    updated link styling

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 echo '<link href="./css/style.css" rel="stylesheet">';
 include ('./functions/functions.php');
+echo '<link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet" type="text/css">';
 
 placeHolder();
 
