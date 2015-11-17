8df8c3967279af9626b6ba72842541db3a881322
SynisterIntent <darksyn@gmail.com>
2015-11-16 18:31:40 -0700

    Test show off
    Added stuff to show gRa

--- functions/functions.php
+++ functions/functions.php
@@ -7,7 +7,7 @@ Function placeHolder(){
 echo '<img id="placeHolder" src="./images/logo.png"/>';
 echo '<br>';
 echo '<span id="coming">Coming Soon Please Check Back or <a href="mailto:info@gdp.photography?subject=Site Inquiry">Inquire Here</a></span>';
-
+echo 'New stuf';
 }
 
 
