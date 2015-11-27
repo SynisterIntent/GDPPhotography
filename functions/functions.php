fbdd05cc84c903b35b8e97d1633fa1feaee51f4d
Jeremy <darksyn@gmail.com>
2015-11-27 15:28:05 -0700

    Mobile detect
    Mobile detect test

--- functions/functions.php
+++ functions/functions.php
@@ -2,9 +2,18 @@
 
 // displays placholder for under construction.
 
-Function placeHolder(){
+Function placeHolder($detect){
+$src;
+if ( $detect->isMobile() ) {
+ $src='./images/logo.png';
+echo '<img id="placeHolder" src="'.$src.'" />';
 
-echo '<img id="placeHolder" src="./images/logo.png"/>';
+} else { 
+	$src='./images/logoMobile.png';
+echo '<img id="Mobile" src="'.$src.'" />';
+
+}
+echo $detect->isMobile();
 echo '<br>';
 echo '<span id="coming">Coming Soon Please Check Back or <a href="mailto:info@gdp.photography?subject=Site Inquiry">Inquire Here</a></span>';
 echo 'New stuf';
--- index.php
+++ index.php
@@ -1,10 +1,16 @@
+<head>
+<script>document.cookie='resolution='+Math.max(screen.width,screen.height)+("devicePixelRatio" in window ? ","+devicePixelRatio : ",1")+'; path=/';</script>
+</head>
 <?php
+require_once 'includes/Mobile_Detect.php';
+$detect = new Mobile_Detect;
+// Any mobile device (phones or tablets).
+
 
 echo '<link href="./css/style.css" rel="stylesheet">';
 include ('./functions/functions.php');
 echo '<link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet" type="text/css">';
-
-placeHolder();
+placeHolder($detect);
 
 
 ?>
